c2910490f9c0d8c83f3d45bc5632f0e3c2e3d182
Manav Jain <manav.mj.jain@gmail.com>
2017-04-25 02:31:23 +0530

    Login screen change

--- app/src/main/java/com/hungryhackers/tweeter/MainActivity.java
+++ app/src/main/java/com/hungryhackers/tweeter/MainActivity.java
@@ -2,27 +2,22 @@ package com.hungryhackers.tweeter;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.res.AssetManager;
-import android.graphics.Color;
-import android.graphics.Typeface;
-import android.os.Parcel;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.twitter.sdk.android.Twitter;
 import com.twitter.sdk.android.core.Callback;
 import com.twitter.sdk.android.core.Result;
 import com.twitter.sdk.android.core.TwitterAuthConfig;
-import com.twitter.sdk.android.core.TwitterAuthToken;
 import com.twitter.sdk.android.core.TwitterException;
 import com.twitter.sdk.android.core.TwitterSession;
 import com.twitter.sdk.android.core.identity.TwitterLoginButton;
-
-import java.util.Locale;
 
 import io.fabric.sdk.android.Fabric;
 
@@ -37,8 +32,12 @@ public class MainActivity extends AppCompatActivity {
 
     TwitterLoginButton button;
 
+    LinearLayout loginButton;
+
     SharedPreferences sp;
     SharedPreferences.Editor editor;
+
+    ImageView twitter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,21 +46,25 @@ public class MainActivity extends AppCompatActivity {
         TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
         Fabric.with(this, new Twitter(authConfig));
 
-//        TwitterSession session = Twitter.getSessionManager().getActiveSession();
-//        sp = getSharedPreferences("Tweeter", MODE_PRIVATE);
-//        editor = sp.edit();
-//        if (session != null) {
-//            if (sp.getBoolean("isLoggedIn", false)){
-//                Intent i = new Intent(MainActivity.this, ScrollingActivity.class);
-//                startActivity(i);
-//                finish();
-//            }else {
-//                editor.putBoolean("isLoggedIn",true);
-//                editor.commit();
-//            }
-//        }
+        TwitterSession session = Twitter.getSessionManager().getActiveSession();
+        sp = getSharedPreferences("Tweeter", MODE_PRIVATE);
+        editor = sp.edit();
+        if (session != null) {
+            if (sp.getBoolean("isLoggedIn", false)){
+                Intent i = new Intent(MainActivity.this, ScrollingActivity.class);
+                startActivity(i);
+                finish();
+            }else {
+                editor.putBoolean("isLoggedIn",true);
+                editor.commit();
+            }
+        }
 
         setContentView(R.layout.activity_main);
+
+        twitter = (ImageView) findViewById(R.id.twitter);
+        Animation fadeIn = AnimationUtils.loadAnimation(this, R.anim.twitter_fade_in);
+        twitter.startAnimation(fadeIn);
 
         button = (TwitterLoginButton) findViewById(R.id.login_button);
         button.setCallback(new Callback<TwitterSession>() {
@@ -69,8 +72,8 @@ public class MainActivity extends AppCompatActivity {
             public void success(Result<TwitterSession> result) {
                 Intent i = new Intent(MainActivity.this, ScrollingActivity.class);
                 startActivity(i);
-//                editor.putBoolean("isLoggedIn",true);
-//                editor.commit();
+                editor.putBoolean("isLoggedIn",true);
+                editor.commit();
                 finish();
             }
 
@@ -79,6 +82,38 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        loginButton = (LinearLayout) findViewById(R.id.login_button_layout);
+        Animation popIn = AnimationUtils.loadAnimation(this, R.anim.login_in);
+        loginButton.startAnimation(popIn);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                button.performClick();
+                Log.i(TAG, "onClick: ");
+
+//                Twitter.logIn(MainActivity.this, new Callback<TwitterSession>() {
+//                    @Override
+//                    public void success(Result<TwitterSession> result) {
+//                        Intent i = new Intent(MainActivity.this, ScrollingActivity.class);
+//                        startActivity(i);
+//                        editor.putBoolean("isLoggedIn",true);
+//                        editor.commit();
+//                        finish();
+//
+//                        Log.i(TAG, "success: ");
+//
+//                    }
+//
+//                    @Override
+//                    public void failure(TwitterException exception) {
+//                        Log.i(TAG, "failure: ");
+//                    }
+//                });
+            }
+        });
+
+
     }
 
     @Override
--- app/src/main/java/com/hungryhackers/tweeter/ProfileFragment.java
+++ app/src/main/java/com/hungryhackers/tweeter/ProfileFragment.java
@@ -1,5 +1,7 @@
 package com.hungryhackers.tweeter;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.AssetManager;
 import android.graphics.Color;
 import android.graphics.Typeface;
@@ -11,11 +13,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.twitter.sdk.android.Twitter;
+
 import java.util.Locale;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 import developer.shivam.library.CrescentoContainer;
 import developer.shivam.library.CrescentoImageView;
+
+import static android.content.Context.MODE_PRIVATE;
 
 /**
  * Created by YourFather on 13-04-2017.
@@ -28,6 +34,8 @@ public class ProfileFragment extends Fragment {
 
     TextView userName;
 
+    TextView logout;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -38,6 +46,19 @@ public class ProfileFragment extends Fragment {
         profileImage = (CircleImageView) v.findViewById(R.id.dp);
 
         userName = (TextView) v.findViewById(R.id.user_name);
+
+        logout = (TextView) v.findViewById(R.id.logout);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences sp = getActivity().getSharedPreferences("Tweeter", MODE_PRIVATE);
+                SharedPreferences.Editor editor = sp.edit();
+                editor.clear().commit();
+                Twitter.logOut();
+                startActivity(new Intent(getActivity(), MainActivity.class));
+                getActivity().finish();
+            }
+        });
 
         AssetManager am = getActivity().getApplicationContext().getAssets();
 
